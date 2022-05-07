d231ae818a4848e1e43e7a47b2ea0150d7606360
Andy <darkcheep@live.com.mx>
2022-05-07 14:10:01 -0500

    Andy implements countdown
    in Scene2. It now transitions to scene3

--- Assets/Scripts/TimerCountdown.cs
+++ Assets/Scripts/TimerCountdown.cs
@@ -12,7 +12,7 @@ public class TimerCountdown : MonoBehaviour
     //Here we stablish that the timer ends when it hits 0
     float currentTime = 0f;
     //starting time in the countdown
-    float startingTime = 105f;
+    float startingTime = 10f;
 
     [SerializeField] TextMeshProUGUI countdownText;
     
@@ -26,6 +26,19 @@ public class TimerCountdown : MonoBehaviour
     {
         //this is how we're making the timer count down
         currentTime -= 1 * Time.deltaTime;
-        print(currentTime);
+        //we override the textUI with our own coutdown:
+        countdownText.text = currentTime.ToString("0.0");
+
+        //what to do when the timer reaches 0
+        if (currentTime <= 0)
+        {
+            //the timer stays at 0
+            currentTime = 0;
+            //and then it loads the next scene
+            SceneManager.LoadScene("Scene3");
+            Debug.Log("Round ended!");
+        }
+
+
     }
 }
